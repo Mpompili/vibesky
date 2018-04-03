41d14f31e5cf855c9f099cb10a167a65355193a6
Michael Pompili <michael@churroboros.com>
2018-04-03 13:24:51 -0700

    create vibesky skeleton

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/vibesky.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};

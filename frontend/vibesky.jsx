c9a42304f69cb7f788d764e15ce23cf32e1811c2
Michael Pompili <michael@churroboros.com>
2018-04-03 18:40:59 -0700

    Create Root & App Components, working root_reducer

--- frontend/vibesky.jsx
+++ frontend/vibesky.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { login, RECEIVE_CURRENT_USER } from './actions/session_actions';
+import { login, logout, RECEIVE_CURRENT_USER } from './actions/session_actions';
 import * as APIUtil from './util/session_api_util';
-
+import Root from './components/root';
 document.addEventListener("DOMContentLoaded", ()=>{
   const root = document.getElementById('root');
   const store = configureStore();
+  ////////////////TEST//////////////////
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-
-  ReactDOM.render(<h1>Welcome to vibesky</h1>, root);
+  window.login = login;
+  window.logout = logout;
+  ////////////////TEST//////////////////
+  ReactDOM.render(<Root store={store} />, root);
 });

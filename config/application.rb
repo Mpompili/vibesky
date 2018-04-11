d2778695396c504e77b5e1d620f05f9abf93085e
Michael Pompili <michael@churroboros.com>
2018-04-11 16:51:28 -0700

    heroku test

--- config/application.rb
+++ config/application.rb
@@ -17,16 +17,15 @@ module Vibesky
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_region => 'us-west-1',
-      :s3_credentials => "#{Rails.root}/config/application.yml"
+      :bucket => ENV["s3_bucket"],
+      :s3_credentials => {
+        :access_key_id => ENV["s3_access_key_id"],
+        :secret_access_key => ENV["s3_secret_access_key"],
+        :s3_region => ENV["s3_region"],
+        :s3_host_name => "s3.#{ENV["s3_region"]}.amazonaws.com"
+      }
     }
 
   end
 end
 
-# {
-#   :bucket => ENV["s3_bucket"],
-#   :access_key_id => ENV["s3_access_key_id"],
-#   :secret_access_key => ENV["s3_secret_access_key"],
-#   :s3_region => ENV["s3_region"],
-#   :s3_host_name => "s3.#{ENV["s3_region"]}.amazonaws.com"
-# }

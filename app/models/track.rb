0738b977a393037bdf19a7bd6ade0ebfdf3c7147
Michael Pompili <michael@churroboros.com>
2018-04-11 16:25:12 -0700

    heroku validation test

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,11 +7,9 @@ class Track < ApplicationRecord
   :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/,
    :message => 'file type is not allowed (only jpeg/png/gif images)'
 
-   has_attached_file :audio,
-                     storage: :s3,
-                     s3_credentials: "app/config/application.yml",
-                     path: "vibesky/:attachment/:style/:id.:extension"
+   has_attached_file :audio, validate_media_type: false
                      
+
  validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg',
    'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3',
    'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
@@ -23,3 +21,8 @@ class Track < ApplicationRecord
 
   has_many :comments, dependent: :destroy 
 end
+
+
+# storage: :s3,
+#                      s3_credentials: "app/config/application.yml",
+#                      path: "vibesky/:attachment/:style/:id.:extension"
